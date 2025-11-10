68cb253363447bad8d6bb66faf1b408a45b19dd6
Juliana Azevedo <julianazevedo09@gmail.com>
2025-11-09 21:18:53 -0300

    feat: add backend produto

---
+++ Projeto/backend/src/routes/controllers/produto.js
@@ -0,0 +1,73 @@
+import pool from "../../../DB/database.js";
+
+
+export async function criarProduto(req, res) {
+  try {
+    const { nome, preco, categoria_id, descricao } = req.body; // pega os dados do body
+
+    console.log("Dados recebidos:", req.body);
+
+    const query = `
+      INSERT INTO produtos (nome, preco, categoria_id, descricao)
+      VALUES ($1, $2, $3, $4)
+      RETURNING id;
+    `;
+    const values = [nome, preco, categoria_id, descricao];
+
+    const result = await pool.query(query, values);
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao criar produtos" });
+  }
+}
+
+export async function listarProduto(req, res) {
+  try {
+    const result = await pool.query("SELECT p.id, p.nome as nomeProduto, p.preco, p.descricao, c.nome as nomeCategoria, c.id as idCategoria FROM produtos p join categorias c on p.categoria_id = c.id");
+    console.log(JSON.stringify(result.rows, null, 2))
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao buscar produtos" });
+  }
+}
+
+export async function atualizarProduto(req, res) {
+  try {
+    const {nome, preco, categoria_id, descricao, id} = req.body
+    const query = `UPDATE produtos SET nome = $1, preco = $2, categoria_id = $3, descricao = $4 WHERE id = $5`
+    const values = [nome, preco, categoria_id, descricao, id]
+
+    const result = await pool.query(query, values);
+
+    if (result.rowCount === 0) {
+      res.status(200).json("Produto não encontrada!")
+    } else {
+       res.status(200).json("Produto alterada com sucesso!")
+    }
+  } catch(error) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao editar nome" });
+  }
+}
+
+export async function deletarProduto(req, res) {
+  try{
+    const {id} = req.body
+    const query = `DELETE FROM produtos WHERE id = $1`
+    const values = [id]
+
+    const result = await pool.query(query, values);
+
+    if (result.rowCount === 0) {
+      res.status(200).json("Produto não encontrada!")
+    } else {
+       res.status(200).json("Produto excluida com sucesso!")
+    }
+  } catch(error) {
+    console.error(err);
+    res.status(500).json({ erro: "Erro ao deletar produtos" });
+  }
+}
